dba490753a02d48b2c48a948d2d0bfc5d9f45772
Dallas Palumbo <palumbodallas@gmail.com>
2023-01-15 12:57:33 -0500

    user controller set up DB connection established

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
+//Postgres Connection string
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try{
+   sequelize.authenticate()
+   console.log("DB connection established")
+}catch(err){
+   console.log(`no dice ${err}`)
+}
+
 //ROOT
  app.get('/', (req,res)=>{
     res.status(200).send("Shop till you drop 🛒")
