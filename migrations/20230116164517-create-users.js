b2ca18a1d1a3c1226fc9c3eabfd09a40ec9f2785
Dallas Palumbo <palumbodallas@gmail.com>
2023-01-16 11:57:11 -0500

    users model created first db migration successful

---
+++ migrations/20230116164517-create-users.js
@@ -0,0 +1,26 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
