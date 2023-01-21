98a2f3d5c662740d06cd6809090bf16412a668d7
Dallas Palumbo <palumbodallas@gmail.com>
2023-01-21 09:47:29 -0500

    trying new config setups

--- config/config.js
+++ config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host":     "127.0.0.1",
-    "port":"5432",
-    "dialect": "postgres"
+    "username": process.env.ELP_USERNAME,
+    "password": process.env.ELP_PASSWORD,
+    "database": process.env.ELP_DATABASE,
+    "host": process.env.ELP_HOST,
+    "dialect": "postgres",
+    "port":"5432"
   },
   "test": {
     "username": "root",
