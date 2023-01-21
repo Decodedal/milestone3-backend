e2da8a6dde4211b50f942e3acf2c99062014e453
Dallas Palumbo <palumbodallas@gmail.com>
2023-01-21 10:37:14 -0500

    added loading spinners

--- config/config.js
+++ config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.ELP_USERNAME,
-    "password": process.env.ELP_PASSWORD,
-    "database": process.env.ELP_DATABASE,
-    "host": process.env.ELP_HOST,
-    "dialect": "postgres",
-    "port":"5432"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host":     "127.0.0.1",
+    "port":"5432",
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
@@ -17,10 +17,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.ELP_USERNAME,
+    "password": process.env.ELP_PASSWORD,
+    "database": process.env.ELP_DATABASE,
+    "host": process.env.ELP_HOST,
     "dialect": "postgres",
     "port":"5432"
   }
